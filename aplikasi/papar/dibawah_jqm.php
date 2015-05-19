c7964a53bd01927480acce08274bf40020bc72cb
Amin PO Muar <helmi007@yahoo.com>
2015-05-19 12:09:18 +0800

    tambah jquery mobile

---
+++ aplikasi/papar/dibawah_jqm.php
@@ -0,0 +1,20 @@
+<?php
+/*
+<!-- Footer
+================================================== -->
+<footer class="footer">
+	<div class="container">
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2014. Theme <
+$theme = ($pilih==null) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null;
+	
+		</span>
+	</div>
+</footer>
+*/
+?>
+<?php require 'jquery.php'; ?>
+
+</body>
+</html>
