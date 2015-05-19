6f15fb86773b0f503fe0f81dd7b2cfd798fefe99
Amin PO Muar <helmi007@yahoo.com>
2015-05-19 12:21:56 +0800

    tambah nota kaki jqm

--- aplikasi/papar/dibawah_jqm.php
+++ aplikasi/papar/dibawah_jqm.php
@@ -14,6 +14,17 @@ echo $theme = (isset($theme)) ? $theme : null;
 </footer>
 */
 ?>
+	<div data-role="footer">
+		<h4>&copy; Hak Cipta Terperihara 2014. </h4>
+		<span>
+		Theme <?php
+$theme = ($pilih==null) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null;
+?>
+		</span>
+	</div>
+</div>
+
 <?php require 'jquery.php'; ?>
 
 </body>
