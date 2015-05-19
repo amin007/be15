c850b3b44370052798a422409aacfd84e899801b
Amin PO Muar <helmi007@yahoo.com>
2015-05-19 12:31:29 +0800

    rapatkan jarak Ad-hock

--- aplikasi/papar/ruangtamu/depan.php
+++ aplikasi/papar/ruangtamu/depan.php
@@ -4,7 +4,7 @@
 		sampai banci ekonomi 2006, saya telah buat sistem kawalan prosesan sepenuhnya
 		secara online. cetak senarai batch pun online sahaja.
 		Jadi sekarang saya dah susun sistem yang saya dah buat mengikut banci/penyiasatan 
-		Tahunan, Bulanan, Suku Tahun, Setengah Tahun dan  Ad-hock.
+		Tahunan, Bulanan, Suku Tahun, Setengah Tahun dan Ad-hock.
 		Harap anda dapat faham tentang sistem yang saya bangunkan menerusi php dan mysql juga mssql.
 		Sekian Wasalam .</p>
 	</div>
