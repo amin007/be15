546a8b0238fec82e34555b01cd89ff35e033cc78
Amin PO Muar <helmi007@yahoo.com>
2015-05-20 16:05:09 +0800

    buang borang carian

--- aplikasi/papar/diatas-jqm.php
+++ aplikasi/papar/diatas-jqm.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title><?php echo !isset($tajuk) ? 'Kosong':$tajuk; ?></title>
+<title><?php echo !isset($this->tajuk) ? 'Kosong':$this->tajuk; ?></title>
 <?php
 //echo $_SERVER['SERVER_NAME'];
 $linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile';
@@ -12,7 +12,8 @@ $linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile';
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 <!-- Include the jQuery Mobile library -->
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-
+			<a href="#" class="ui-btn ui-shadow ui-corner-all ui-icon-search ui-btn-icon-notext">Search</a>
+			echo "\n"; include 'menubar-jqm.php'; echo "\n";
 */
 ?>
 <!-- Include meta tag to ensure proper rendering and touch zooming -->
@@ -26,11 +27,6 @@ $linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile';
 </head>
 <body>
 <div data-role="page" id="home">
-	<div data-role="header">
-		<h1><?php echo !isset($tajuk) ? 'Kosong':$tajuk; ?></h1><?php 
-		echo "\n"; include 'menubar-jqm.php'; echo "\n"; ?>
-		<form method="post" action="#">
-			<input type="search" name="cariNama" id="cariNama" 
-			placeholder="Cari Newss/Nama" data-clear-btn="true">
-		</form>
+	<div data-role="header" class="header">
+<?php echo "\n"; include 'menubar-jqm.php'; echo "\n"; ?>
 	</div>
