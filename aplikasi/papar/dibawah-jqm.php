f5b985de3d25f82b9db20e59fd76bdf8f411f482
Amin PO Muar <helmi007@yahoo.com>
2015-05-20 08:20:24 +0800

    buang koding lama dalam bootstrap twitter

--- aplikasi/papar/dibawah-jqm.php
+++ aplikasi/papar/dibawah-jqm.php
@@ -1,19 +1,5 @@
-<?php
-/*
 <!-- Footer
 ================================================== -->
-<footer class="footer">
-	<div class="container">
-		<span class="label label-info">
-		&copy; Hak Cipta Terperihara 2014. Theme <
-$theme = ($pilih==null) ? 'Asal Bootstrap Twitter' : $pilih;
-echo $theme = (isset($theme)) ? $theme : null;
-	
-		</span>
-	</div>
-</footer>
-*/
-?>
 	<div data-role="footer">
 		<h4>&copy; Hak Cipta Terperihara 2014. </h4>
 		<p><span>
