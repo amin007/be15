3c3cd9a3d4e4bf6dac45e0f8bc7b3d98d7393f6b
Amin PO Muar <helmi007@yahoo.com>
2015-05-20 16:12:59 +0800

    buat form untuk carian newss/nama syarikat. wujudkan fungsi cari, cariNama, semakData.

--- aplikasi/kawal/mobile.php
+++ aplikasi/kawal/mobile.php
@@ -6,7 +6,8 @@ class Mobile extends Kawal
 	function __construct() 
 	{
 		parent::__construct();
-        //Kebenaran::kawalMasuk();		
+        //Kebenaran::kawalMasuk();
+		$this->papar->tajuk = 'SSE 2015';
 	}
 	
 	function index() 
@@ -21,4 +22,45 @@ class Mobile extends Kawal
 		$this->papar->baca('mobile/iconjqm');
 	}
 
+	function cari() 
+	{
+		// pergi papar kandungan
+		$this->papar->baca('mobile/cari');
+	}
+
+	function cariNama() 
+	{
+		// pergi papar kandungan
+		$this->papar->baca('mobile/carinama');
+	}
+
+	function carian() 
+	{
+		//$cariNama = $this->semakData(bersih($_POST['cariNama']));
+		$cariNama = $_POST['cariNama'];
+		
+		echo (is_numeric($cariNama)) ?
+			"{$cariNama} adalah newss"
+			: "{$cariNama} adalah nama";
+		// pergi papar kandungan
+		//$this->papar->baca('mobile/cari');
+	}
+// function yang bukan dicapai secara terus dari URL	
+	function semakData($cariNama) 
+	{	
+		if (is_numeric($cariNama)):
+			$carian = $cariNama;
+		else:
+			$carian = $cariNama;
+		endif;
+	
+		return $carian;
+		
+	}
+	
+	function cariDB($cariNama)
+	{
+
+
+	}
 }
