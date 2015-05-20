68eda62c2f8a33035edff9134ce1cfa6dfd67c0c
Amin PO Muar <helmi007@yahoo.com>
2015-05-20 13:23:39 +0800

    tambah menubar-jqm.php

---
+++ aplikasi/papar/menubar-jqm.php
@@ -0,0 +1,9 @@
+		<div data-role="navbar">
+			<ul>
+				<li><a href="<?php echo URL ?>mobile" data-icon="home">Anjung</a></li>
+				<li><a href="<?php echo URL ?>mobile/icon" data-icon="bullets">Kawal</a></li>
+				<li><a href="<?php echo URL ?>mobile/cari" data-icon="search">Cari</a></li>
+				<li><a href="#anylink" data-icon="grid">Laporan</a></li>
+				<li><a href="<?php echo URL ?>ruangtamu/logout" data-icon="lock">Keluar</a></li>
+			</ul>
+		</div>
