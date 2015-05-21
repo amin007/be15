e700f63eca56cfbe6d4395d4d88d658d63ec90f1
Amin PO Muar <helmi007@yahoo.com>
2015-05-21 13:37:30 +0800

    tukar tahun

--- aplikasi/papar/dibawah-jqm.php
+++ aplikasi/papar/dibawah-jqm.php
@@ -1,14 +1,12 @@
 <!-- Footer
 ================================================== -->
 	<div data-role="footer" style="text-align:center;">
-		<h4>&copy; Hak Cipta Terperihara 2014. </h4>
-		<span>
-		Theme <?php
+		<h4>&copy; Hak Cipta Terperihara 2015. </h4>
+		<span> Theme <?php
 $pilih = ( isset($pilih) ) ? $pilih : null;
 $theme = ($pilih==null) ? 'Asal Jquery Mobile' : $pilih;
 echo $theme = (isset($theme)) ? $theme : null;
-?>
-		</span>
+?>	</span>
 	</div>
 </div>
 
