88fe206e5b92ad7dad0476e2566a779dbf1fa41c
Amin PO Muar <helmi007@yahoo.com>
2015-05-21 11:24:03 +0800

    buat pembolehubah $linkJS

--- aplikasi/papar/diatas-jqm.php
+++ aplikasi/papar/diatas-jqm.php
@@ -4,7 +4,7 @@
 <title><?php echo !isset($this->tajuk) ? 'Kosong':$this->tajuk; ?></title>
 <?php
 //echo $_SERVER['SERVER_NAME'];
-$linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile';
+$linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile/';
 /*
 <!-- Include jQuery Mobile stylesheets -->
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
@@ -12,18 +12,17 @@ $linkJS = $_SERVER['SERVER_NAME'] . '/private_html/js/jquery/mobile';
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 <!-- Include the jQuery Mobile library -->
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-			<a href="#" class="ui-btn ui-shadow ui-corner-all ui-icon-search ui-btn-icon-notext">Search</a>
 			echo "\n"; include 'menubar-jqm.php'; echo "\n";
 */
 ?>
 <!-- Include meta tag to ensure proper rendering and touch zooming -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Include jQuery Mobile stylesheets -->
-<link rel="stylesheet" href="http://<?php echo $linkJS ?>/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="http://<?php echo $linkJS ?>jquery.mobile-1.4.5.min.css">
 <!-- Include the jQuery library -->
-<script src="http://<?php echo $linkJS ?>/jquery-1.11.2.min.js"></script>
+<script src="http://<?php echo $linkJS ?>jquery-1.11.2.min.js"></script>
 <!-- Include the jQuery Mobile library -->
-<script src="http://<?php echo $linkJS ?>/jquery.mobile-1.4.5.min.js"></script>
+<script src="http://<?php echo $linkJS ?>jquery.mobile-1.4.5.js"></script>
 </head>
 <body>
 <div data-role="page" id="home">
