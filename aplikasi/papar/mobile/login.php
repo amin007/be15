8fe6214b199a468e85cb34549f97e5b3bd2f9624
Amin PO Muar <helmi007@yahoo.com>
2015-06-09 12:58:43 +0800

    1. align kod ikut logik. 2. tukar name=user => username dan name=pass => password 3. demo-html kepada data-demo-html

--- aplikasi/papar/mobile/login.php
+++ aplikasi/papar/mobile/login.php
@@ -15,34 +15,36 @@ if ( in_array($this->ip2,$this->senaraiIP) )
 		data-icon="check" data-theme="a" data-transition="flip"><?php echo $fe ?></a>
 <?php 
 	endforeach; 
-$kakitangan = senarai_kakitangan();
-$fe = $key = null;
-foreach ($kakitangan as $key => $fe):  
-	$imej = 'http://' . $_SERVER['SERVER_NAME'] . '/private_html/bg/kakitangan/' . $fe . '.jpg';
-	$nama = ($fe=='amin') ? 'amin007' : $fe;
-	$password = ($fe=='amin') ? null : $fe;
+	$kakitangan = senarai_kakitangan();
+	$fe = $key = null;
+	foreach ($kakitangan as $key => $fe):  
+		$imej = 'http://' . $_SERVER['SERVER_NAME'] . '/private_html/bg/kakitangan/' . $fe . '.jpg';
+		$nama = ($fe=='amin') ? 'amin007' : $fe;
+		$password = ($fe=='amin') ? null : $fe;
 ?>
-
 	<div data-demo-html="true">	
 	<div data-role="popup" id="popupMenu" data-theme="a">
 	<div data-role="popup" id="<?php echo $fe ?>" data-theme="a" class="ui-corner-all">
 		<form data-ajax="false" method="POST" action="login/semakid">
 		<div style="padding:10px 20px;">
-			<h3>Please sign in</h3>
 			<img src="<?php echo $imej ?>">
 			<label for="un" class="ui-hidden-accessible">Nama Anda:</label>
-				<input type="text" name="user" id="un" value="<?php echo $nama ?>" placeholder="username" data-theme="a" />
+				<input type="text" name="username" id="un" value="<?php echo $nama ?>" placeholder="Nama Anda" data-theme="a" />
 			<label for="pw" class="ui-hidden-accessible">Kata Laluan:</label>
-				<input type="password" name="pass" id="pw" value="<?php echo $password ?>" placeholder="password" data-theme="a" />
+				<input type="password" name="password" id="pw" value="<?php echo $password ?>" placeholder="Kata Laluan" data-theme="a" />
 			<input type="submit" name="masuk" value="Masuk" data-theme="b" data-icon="check">
 			<!-- <button type="submit" data-theme="b" data-icon="check">Sign in</button> -->
 		</div>
 		</form>
 	</div>
 	</div>
-	</div><!--/demo-html -->
-<?php endforeach; 
+	</div><!--/data-demo-html -->
+<?php 
+	endforeach; 
 }
-else{echo 'ip anda ' . $ip . ', anda tiada kebenaran masuk sistem';}
+else
+{	
+	echo 'ip anda ' . $ip . ', anda tiada kebenaran masuk sistem';
+}
 
 ?>
