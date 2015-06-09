e268399a6ae3f1e89f9789f5d3152a327cea958e
Amin PO Muar <helmi007@yahoo.com>
2015-06-09 14:03:33 +0800

    1. tambah link untuk icon. 2. tambah data-ajax="false" untuk link logout

--- aplikasi/papar/menubar-jqm.php
+++ aplikasi/papar/menubar-jqm.php
@@ -2,6 +2,7 @@
 		<div data-role="navbar">
 			<ul>
 				<li><a href="<?php //echo URL ?>#mobile" data-icon="home">Anjung</a></li>
+				<li><a href="<?php echo URL ?>mobile/icon" data-icon="bullets">Icon</a></li>
 			</ul>
 		</div>
 <?php else:?>
@@ -11,7 +12,7 @@
 				<li><a href="<?php echo URL ?>mobile/icon" data-icon="bullets">Kawal</a></li>
 				<li><a href="<?php echo URL ?>mobile/cari" data-icon="search">Cari</a></li>
 				<li><a href="#anylink" data-icon="grid">Laporan</a></li>
-				<li><a href="<?php echo URL ?>ruangtamu/logout" data-icon="lock">Keluar</a></li>
+				<li><a href="<?php echo URL ?>ruangtamu/logout" data-icon="lock" data-ajax="false">Keluar</a></li>
 			</ul>
 		</div>
 <?php endif;?>
