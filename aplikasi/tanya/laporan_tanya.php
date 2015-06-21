bbf22050abe3a894a6820a1911653d4ced1bc5b6
Amin PO Muar <helmi007@yahoo.com>
2015-06-21 15:49:05 +0800

    buang fungsi yang tiada kaitan lagi

--- aplikasi/tanya/laporan_tanya.php
+++ aplikasi/tanya/laporan_tanya.php
@@ -567,26 +567,5 @@ class Laporan_Tanya extends Tanya
 		
 		return $result;
 	}
-*/
-	public function xhrInsert() 
-	{
-		$text = $_POST['text'];
-		$this->db->insert('data', array('text' => $text));
-		$data = array('text' => $text, 'id' => $this->db->lastInsertId());
-		echo json_encode($data);
-	}
-	
-	public function xhrGetListings()
-	{
-		$result = $this->db->select("SELECT * FROM data");
-		//echo $result;
-		echo json_encode($result);
-	}
-	
-	public function xhrDeleteListing()
-	{
-		$id = (int) $_POST['id'];
-		$this->db->delete('data', "id = '$id'");
-	}
-
+//*/
 }
