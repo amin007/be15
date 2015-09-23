eaa24752ca9dc9099ea541d1631289676756f8b3
Amin PO Muar <helmi007@yahoo.com>
2015-09-23 16:46:00 +0800

    ringkaskan kod js

--- aplikasi/kawal/kawalan.php
+++ aplikasi/kawal/kawalan.php
@@ -8,24 +8,12 @@ class Kawalan extends Kawal
         Kebenaran::kawalKeluar();
         
         $this->papar->js = array(
-            //'bootstrap.js',
-            'bootstrap-transition.js',
-            'bootstrap-alert.js',
-            'bootstrap-modal.js',
-            'bootstrap-dropdown.js',
-            'bootstrap-scrollspy.js',
-            'bootstrap-tab.js',
-            'bootstrap-tooltip.js',
-            'bootstrap-popover.js',
-            'bootstrap-button.js',
-            'bootstrap-collapse.js',
-            'bootstrap-carousel.js',
-            'bootstrap-typeahead.js',
-            'bootstrap-affix.js',
-            'bootstrap-datepicker.js',
-            'bootstrap-datepicker.min.js',
-            'bootstrap-datepicker.ms.js',
-            'bootstrap-editable.min.js');
+            /*'bootstrap-transition.js','bootstrap-alert.js','bootstrap-modal.js','bootstrap-dropdown.js',
+			'bootstrap-scrollspy.js','bootstrap-tab.js','bootstrap-tooltip.js','bootstrap-popover.js',
+			'bootstrap-button.js','bootstrap-collapse.js','bootstrap-carousel.js','bootstrap-typeahead.js',
+			'bootstrap-affix.js',*/
+			'bootstrap-datepicker.js','bootstrap-datepicker.min.js',
+			'bootstrap-datepicker.ms.js','bootstrap-editable.min.js');
         $this->papar->css = array(
             'bootstrap-datepicker.css',
             'bootstrap-editable.css');			
