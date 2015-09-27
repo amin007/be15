d4cac01779f14ec4883bc80455282a514e8957ef
Amin PO Muar <helmi007@yahoo.com>
2015-09-27 16:22:58 +0800

    buang kod tiada kaitan

--- aplikasi/tanya/kawalan_tanya.php
+++ aplikasi/tanya/kawalan_tanya.php
@@ -406,25 +406,5 @@ class Kawalan_Tanya extends Tanya
 		return $result;
 	}
 */
-	public function xhrInsert() 
-	{
-		$text = $_POST['text'];
-		$this->db->insert('data', array('text' => $text));
-		$data = array('text' => $text, 'id' => $this->db->lastInsertId());
-		echo json_encode($data);
-	}
-	
-	public function xhrGetListings()
-	{
-		$result = $this->db->select("SELECT * FROM data");
-		//echo $result;
-		echo json_encode($result);
-	}
-	
-	public function xhrDeleteListing()
-	{
-		$id = (int) $_POST['id'];
-		$this->db->delete('data', "id = '$id'");
-	}
-
+#####################################################################################
 }
